b1a6445444ffd56fb6e4832d825e6cb7d337f517
CodeDump443 <raliia.0118z@gmail.com>
2025-06-07 09:46:22 +0500

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,130 @@
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxAccessibility,
+  RxHome,
+  RxPerson,
+  RxDashboard,
+} from "react-icons/rx";
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/assets/discord.svg",
+    href: "https://discordapp.com/users/793005781871099984"
+  },
+  {
+    name: "Facebook",
+    src: "/assets/facebook.svg",
+    href: "https://www.facebook.com/profile.php?id=61577070903429"
+  },
+  {
+    name: "Instagram",
+    src: "/assets/instagram.svg",
+    href: "https://www.instagram.com/raliazzzzzz/"
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "Home",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "About me",
+    icon: RxPerson,
+    link: "/about-me",
+  },
+  {
+    name: "Projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+];
+
+export const ProImages = [
+  {
+    src: "/assets/space/asylim1.jpeg",
+  },
+  {
+    src: "/assets/space/asylim2.jpeg",
+  },
+  {
+    src: "/assets/space/asylim3.jpeg",
+  },
+  {
+    src: "/assets/space/asylim4.jpeg",
+  },
+];
+
+export const ProImages2 = [
+  {
+    src: "/assets/space/beacon1.jpeg",
+  },
+  {
+    src: "/assets/space/beacon2.jpeg",
+  },
+  {
+    src: "/assets/space/beacon3.jpeg",
+  },
+  {
+    src: "/assets/space/beacon4.jpeg",
+  },
+];
+
+export const ServiceData = [
+  {
+    icon: RxReader,
+    title: "Mastery",
+    content: "Изучение движков",
+    backgroundImage: "/assets/space/game-engines.png",
+  },
+  {
+    icon: RxCrop,
+    title: "Development",
+    content: "Разработка",
+    backgroundImage: "/assets/space/prototype.png",
+  },
+  {
+    icon: RxPencil2,
+    title: "Narrative",
+    content: "Написание качественной истории",
+    backgroundImage: "/assets/space/narrative.png",
+  },
+  {
+    icon: RxDesktop,
+    title: "Systems",
+    content: "Проектирование логики",
+    backgroundImage: "/assets/space/design.png",
+  },
+  {
+    icon: RxAccessibility,
+    title: "Experience",
+    content: "Формирование пользовательского опыта",
+    backgroundImage: "/assets/space/game-ux.png",
+  },
+  {
+    icon: RxRocket,
+    title: "Vision",
+    content: "Создание своего собственного стиля",
+    backgroundImage: "/assets/space/visual-styles1.jpg",
+  },
+]
+export const Projects = {
+asylim: [
+	{ id: 1, title: "asylim1", description: "Описание Asylim 1", image: "/assets/space/asylim1.jpeg" },
+   	{ id: 2, title: "asylim2", description: "Описание Asylim 2", image: "/assets/space/asylim2.jpeg" },
+   	{ id: 3, title: "asylim3", description: "Описание Asylim 3", image: "/assets/space/asylim3.jpeg" },
+   	{ id: 4, title: "asylim4", description: "Описание Asylim 4", image: "/assets/space/asylim4.jpeg" },
+],
+beacon: [
+	{ id: 5, title: "Beacon 1", description: "Описание Beacon 1", image: "/assets/space/beacon1.jpeg" },
+  	{ id: 6, title: "Beacon 2", description: "Описание Beacon 2", image: "/assets/space/beacon2.jpeg" },
+  	{ id: 7, title: "Beacon 3", description: "Описание Beacon 3", image: "/assets/space/beacon3.jpeg" },
+  	{ id: 8, title: "Beacon 4", description: "Описание Beacon 4", image: "/assets/space/beacon4.jpeg" },
+]
+}
