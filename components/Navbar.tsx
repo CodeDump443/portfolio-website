75ae9692ccad0888fb71df7b1c7eb29476d97afa
CodeDump443 <raliia.0118z@gmail.com>
2025-06-09 01:53:28 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="fixed top-0 bg-transparent z-[20] w-full flex gap-5 md:justify-between md:px-60 p-5">
-      <h1 className="text-white text-[24px] sm:text-[32px] md:text-[38px]">
+      <h1 className="text-white text-[38px]">
         <span className="font-thin">Frame of </span>
         <span className="font">Thought</span>
         <span className="text-blue-500">.</span>
